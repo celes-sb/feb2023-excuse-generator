016d38ea349f9a4ab4bdb3176d8277ec2d56547e
Celeste Bareiro <cnyx7@proton.me>
2023-02-07 02:46:59 +0000

    updated Excuse Generator, done a second time

--- src/app.js
+++ src/app.js
@@ -4,8 +4,29 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { arrow } from "@popperjs/core";
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+const excuse = () => {
+  let resultado = who[Math.floor(Math.random() * who.length)] + " ";
+  resultado += action[Math.floor(Math.random() * action.length)] + " ";
+  resultado += what[Math.floor(Math.random() * what.length)] + " ";
+  resultado += when[Math.floor(Math.random() * when.length)];
+  return resultado;
+};
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excusaFinal = document.querySelector("#excuse");
+  excusaFinal.innerHTML = excuse();
 };
